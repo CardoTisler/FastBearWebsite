513a6784a40acdb00f4e555d7f7bdbda19adb1d8
Cardo Tisler <cardotisler@live.com>
2019-05-26 17:00:58 +0300

    Fourth commit.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var KeevitusText = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
 var MetallitöötlusText = "<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>"
 var CustomDetailidText = "<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia.</p>"
 var DetailideTöötlusText = "<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.</p>"
+var teenused = $("#teenused-list-text li");
 function changeText(index){
     var teenusedText = $("#teenused-description");
     if(index == 0){
@@ -15,7 +16,17 @@ function changeText(index){
     } else if (index == 3){
         teenusedText.html(DetailideTöötlusText);
     }
+
+    highlightTeenusedElement(index);
 }
+function highlightTeenusedElement(index){
+    teenused.removeClass();
+    teenused.eq(index).addClass("teenused-list-active-element");
+}
+//set default highlighted element and default text
+changeText(0);
+highlightTeenusedElement(0);
+
 
 //Galerii Lightbox script
 function expandPic(imageNumber){
@@ -61,7 +72,7 @@ function prevPicture(){
 $(document).ready(function(){
     $(".hover-class").hover(function(){
         $(this).animate({
-            "border-bottom-width":"2px"      
+            "border-bottom-width":"1px"      
         }, 100);
     });
 });
@@ -85,3 +96,4 @@ $(document).ready(function(){
         "opacity" : "1"
     }, 4000)
 });
+
